b172fb3cf1af228b551825d43267738df5a20657
do-or-do-not <danieltuohy@Daniels-MacBook-Air.local>
2015-03-21 11:06:55 +0000

    Dan_Starting to dig deeper into variables

--- Titanic_getting started with R tutorials.R
+++ Titanic_getting started with R tutorials.R
@@ -53,6 +53,7 @@ test$Survived <- 0
 # this command is the same as rep() function, we are adding in all 0's to this column
 
 test$Survived[test$Sex == 'female'] <- 1
+# the double equals is a boolean test
 # we are now replacing the 0's with ones were the sex is equal to females
 # we seen earlier that the majority of females survived.
 
@@ -68,6 +69,80 @@ prop.table(table(train$Age, train$Survived),1)
 prop.table(table(train$Pclass, train$Survived),1)
 
 # How can you set combinations of attributes equal to some value i.e. 1 or 0?
+
+summary(train$Age)
+
+# We can see from this command that the average age is 29 and we have 177 missing values
+
+train$Child <- 0
+train$Child[train$Age < 5] <- 1
+
+# proportion tables become less useful for continous variables as we could have only two 
+# instances of each age. # we can create a new child variable
+# the less than sign is another boolean test..NA's will also fail test and give a 0
+# this is Ok for now because we can assume that all NA's are the average age of 29 to
+# keep things simple.
+
+aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
+# This command will subset the whole dataframe on 'Child' and 'Sex'
+# This command will then sum the target variable 'Survived' for each subset
+# It will give us the suvivors for both is 'Child' and not 'Child'.
+
+aggregate(Survived ~ Child + Sex, data=train, FUN=length)
+# aggregate(Pclass ~ Child + Sex, data=train, FUN=length)
+# This command will subset the whole dataframe on 'Child' and 'Sex'
+# This command will then get the length of the target variable 'Survived' or Survived vector
+# for each subset. In other words it will count the number of people for each subset regardless
+# if any of them are 1's or 0's.
+# This command would have equally worked using another variable as the target variable such as Pclass
+
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+# This command peforms two functions on the subsets which works out the proportion
+# we have the number of survivors sum(x) divided by the total number of people length(x)
+
+# We find out that 75% of female children (<18) survive & 39% of male children (<18) survive
+# WE COULD PERHAPS LOOK AT YOUNGER AGES AS A BETTER DEFINTION of a child to see the effect.
+
+# THIS MAKES A BIG DIFFERENCE TO THE PROPORTIONS
+# We find out that 74% of female children (<5) survive & 65% of male children (<5) survive
+
+### TRY ANOTHER SIMPLE MODEL - WOMAN FIRST AND CHILDREN #####
+
+test$Survived <- 0
+# this command is the same as rep() function, we are adding in all 0's to this column
+# aggregate(Survived ~ Child + Sex, data=test, FUN=length)
+
+test$Child <- 0
+test$Child[test$Age < 5] <- 1
+test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Age < 5] <- 1
+# this command should do the trick for assigning all females and children under the age of 5 as survived
+
+test
+# do this to check that the correct values were assigned
+
+submit_womanandchildfirst <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+
+write.csv(submit_womanandchildfirst, file = "womanandchildfirst.csv", row.names = FALSE)
+
+# THIS PREDICTOR DID NOT IMPROVE MY SCORE. I PUT THIS DOWN TO THE SMALL NUMBER OF MALE CHILDREN
+# UNDER THE AGE OF 5 IN THE TEST SET (I THINK IT IS 4)
+
+#### Start to look at class variable #####
+
+train$Fare2 <- '30+'
+# this creates a new variable and assigns the value of '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+# these commands will break the remaining fares into different groups
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+# the aggregate() function will work out the proportion of survivors within each data subset
+
+
+
+
 
 
 
